b52e866caec0f5b4267f8f6b2329562370d02a96
joss1015 <jossems.1012@gmail.com>
2023-07-28 04:03:54 -0600

    Update Crear Tabla Transaction

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -6,8 +6,8 @@ const postgresql = newDb();
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
                 insert into users values (123, 'Juan', true);`);
 
-postgresql.public.none(`create table transaction(pk_transaction integer, fk_user integer,  description text, amount float);
-                insert into transaction values (1, 123, 'viaje', 500);`);
+postgresql.public.none(`create table transactions(pk_transaction integer, fk_user integer,  description string, amount float);
+                insert into transactions values (1, 123, 'viaje', 500);`);
                 
 module.exports = {
     postgresql
