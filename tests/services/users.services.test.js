8e32f2a317c58ae3a02f28dd5662eaef57fa4103
joss1015 <jossems.1012@gmail.com>
2023-07-28 03:23:16 -0600

    Agregar unit test para service users
    Se debe agregar el test para la funcion de updateUser y deleteUser que se crearon en las tareas anteriores
    Ejecutar el script test de jest para validar el unit testing
    ver referencias, pg-mem

--- tests/services/users.services.test.js
+++ tests/services/users.services.test.js
@@ -2,8 +2,8 @@ const users = require('../../services/users')
 
 describe('users main functions', () => {
     // createUser function test
-    test('createUser with {pk_user: 5, name: "Carlos"}', async () => {
-        let user = await users.createUser(5, "Carlos");
+    test('createUser with {pk_user: 5, name: "Carlos", status: true}', async () => {
+        let user = await users.createUser(5, "Carlos", true);
         expect(user.pk_user).toBe(5);
     });
 
@@ -11,5 +11,18 @@ describe('users main functions', () => {
     test('getUser with {pk_user: 123}', async () => {
         let user = await users.getUser(123)
         expect(user.pk_user).toBe(123);
+    });
+
+    //updateUser function test
+    test('updateUser with {pk_user: 5, name: "Totto", status: true}', async () => {
+        let user = await users.updateUser(5, "Totto", true);
+        expect(user.name).toBe("Totto");
+    });
+
+
+    // deleteUser function test
+    test('deleteUser with {pk_user: 123}', async () => {
+        let user = await users.deleteUser(123)
+        expect(user.pk_user).not.toBe(123)
     });
 })
